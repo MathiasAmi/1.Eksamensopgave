7a3cdbec01922a52a6a3a582982e111e73e4c89f
MathiasAmi <math35i0@edu.easj.dk>
2018-12-18 15:31:08 +0100

    TV2 Media Manager comments and diagram Nedarvning Proevede at få GUI til at virke men uden held

--- src/Controller.java
+++ src/Controller.java
@@ -1,7 +1,21 @@
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+
+import java.awt.*;
 
 public class Controller {
 
     @FXML
+    private TextField textField;
 
+
+
+
+    public void readMediaFolder(ActionEvent actionEvent) {
+        Main.readMediaFolder(Main.MEDIA_MAPPE);
+
+        textField.setText("hej");
+
+
+    }
 }
